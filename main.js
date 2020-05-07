74450bd726715da9206d69fedcfa3ae10d4c193e
dol.jpo <dol.jpo@hotmail.com>
2020-05-07 08:30:50 -0300

    Add conditionally 'ON SALE' span

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var app = new Vue({
     el: '#app',
     data:  {
         product: 'Socks',
-        description: 'White socks',
         image: './assets/socks.jpg',
-        imageLink: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+        imageLink: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        onSale: true
     }
 })
