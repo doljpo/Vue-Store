b59c4d503fed4448ee9624ca5a7c966e490e2da3
dol.jpo <dol.jpo@hotmail.com>
2020-05-10 21:44:17 -0300

    Add more details/variants to product

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
 var app = new Vue({
     el: '#app',
-    data:  {
+    data: {
         product: 'Socks',
-        image: './assets/socks.jpg',
+        image: './assets/green-socks.jpg',
         imageLink: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-        onSale: true
+        onSale: true,
+        details: [
+            "80% cotton",
+            "20% polyester",
+            "Gender-neutral"
+        ],
+        variants: [
+            {
+                variantId: 1234,
+                variantColor: 'Green',
+                variantImage: './assets/green-socks.jpg'
+            },
+            {
+                variantId: 4321,
+                variantColor: 'Blue',
+                variantImage: './assets/blue-socks.jpg'
+            }
+        ],
+        sizes: ["P", "M", "G"],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart++;
+        },
+
+        removeFromCart() {
+            if (this.cart > 0)
+                this.cart--;
+        },
+
+        showProductVariant(variantColor) {
+            this.image = variantColor;
+        }
     }
 })
+
+
+//https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg
