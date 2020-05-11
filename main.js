d32c812b42697550dc38e1b5a274661de3638d06
dol.jpo <dol.jpo@hotmail.com>
2020-05-10 23:48:42 -0300

    Add conditional style

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './assets/green-socks.jpg',
-        imageLink: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        brand: 'Vue Mastery',
         onSale: true,
         details: [
             "80% cotton",
@@ -14,16 +13,38 @@ var app = new Vue({
             {
                 variantId: 1234,
                 variantColor: 'Green',
-                variantImage: './assets/green-socks.jpg'
+                variantImage: './assets/green-socks.jpg',
+                variantImageLink: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 4321,
                 variantColor: 'Blue',
-                variantImage: './assets/blue-socks.jpg'
+                variantImage: './assets/blue-socks.jpg',
+                variantImageLink: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0
             }
         ],
+        selectedVariant: 0,
         sizes: ["P", "M", "G"],
         cart: 0
+    },
+    computed: {
+        cartIsEmpty() {
+            return this.cart <= 0;
+        },
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        imageLink() {
+            return this.variants[this.selectedVariant].variantImageLink;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        }
     },
     methods: {
         addToCart() {
@@ -35,8 +56,9 @@ var app = new Vue({
                 this.cart--;
         },
 
-        showProductVariant(variantColor) {
-            this.image = variantColor;
+        showProductVariant(index) {
+            this.selectedVariant = index;
+            console.log(this.selectedVariant);            
         }
     }
 })
